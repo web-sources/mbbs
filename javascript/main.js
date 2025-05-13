0f78e7cd7aa53a4a4858fdfea0239a0755c6d5b9
Sagarmee <sagarmodi1331@gmail.com>
2025-05-13 10:05:04 +0530

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,550 @@
+
+//owl-product
+
+/*scrolling banner*/
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        $(".owl-carousel-one").owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplaySpeed: 1000,
+            smartSpeed: 1500,
+            autoplayHoverPause: true,
+        });
+    });
+
+    $(document).ready(function () {
+        $(".owl-carousel-two").owlCarousel({
+            items: 4,
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplaySpeed: 1000,
+            smartSpeed: 1500,
+            autoplayHoverPause: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                480: {
+                    items: 2,
+                },
+                767: {
+                    items: 3,
+                },
+                992: {
+                    items: 3,
+                },
+                1200: {
+                    items: 4,
+                },
+            },
+        });
+    });
+
+    $(document).ready(function () {
+        $(".owl-carousel-three").owlCarousel({
+            items: 4,
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplaySpeed: 1000,
+            smartSpeed: 1500,
+            autoplayHoverPause: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                480: {
+                    items: 2,
+                },
+                767: {
+                    items: 3,
+                },
+                992: {
+                    items: 3,
+                },
+                1200: {
+                    items: 4,
+                },
+            },
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-de").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+    $(document).ready(function () {
+        $("#sample-carousel").owlCarousel({
+            items: 5,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-2").owlCarousel({
+            items: 2,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 2
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-4").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-5").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-6").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+
+    $(document).ready(function () {
+        $("#news-slider-7").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+
+    $(document).ready(function () {
+        $("#news-slider-8").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-9").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-10").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-11").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("#news-slider-12").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+            }
+        });
+    });
+})(jQuery);
+
+
+const nav = document.querySelector(".nv")
+const hamburger = document.querySelector(".hamburger")
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active")
+    nav.classList.toggle("active")
+})
+
+//scroll
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+        $(".scrollup").fadeIn();
+    } else {
+        $(".scrollup").fadeOut();
+    }
+})
+
+$(".scrollup").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+})
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+try {
+    fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function (response) {
+        return true;
+    }).catch(function (e) {
+        var carbonScript = document.createElement("script");
+        carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+        carbonScript.id = "_carbonads_js";
+        document.getElementById("carbon-block").appendChild(carbonScript);
+    });
+} catch (error) {
+    console.log(error);
+}
+
+//counter
+
+
+(function ($) {
+    "use strict";
+    function count($this) {
+        var current = parseInt($this.html(), 10);
+        current = current + 1; /* Where 50 is increment */
+        $this.html(++current);
+        if (current > $this.data('count')) {
+            $this.html($this.data('count'));
+        } else {
+            setTimeout(function () { count($this) }, 5);
+        }
+    }
+    $(".stat-count").each(function () {
+        $(this).data('count', parseInt($(this).html(), 10));
+        $(this).html('0');
+        count($(this));
+    });
+})(jQuery);
+
+
+
+/*
+
+.item {
+    opacity: 0.4;
+    transition: 0.4s ease all;
+    margin: 0 20px;
+    transform: scale(0.8);
+  }
+  @media (max-width: 1000px) {
+    .item {
+      margin: 0;
+      transform: scale(0.9);
+    }
+  }
+  .active .item {
+    opacity: 1;
+    transform: scale(1);
+  }
+  body {
+    padding: 0;
+    margin: 80px 0 0 0;
+    font-family: Merriweather;
+    background: #f0e8d5;
+  }
+  
+  .owl-item {
+    -webkit-backface-visibility: hidden;
+    -webkit-transform: translateZ(0) scale(1, 1);
+  }
+  
+  .inner {
+    position: absolute;
+    bottom: 30px;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
+  .inner a {
+    color: #fff;
+    text-decoration: none;
+    border-bottom: 2px solid rgba(255, 255, 255, 0.5);
+    transition: 0.3s ease border-color;
+  }
+  .inner a:hover {
+    border-color: #fff;
+  }
+  .black .inner a {
+    color: #000;
+    border-color: rgba(0, 0, 0, 0.4);
+  }
+  .black .inner a:hover {
+    border-color: #000;
+  }
+  
+  .owl-carousel:after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 8%;
+    top: 0;
+    bottom: 0;
+    left: 50%;
+    margin-left: -4%;
+    pointer-events: none;
+    background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/43033/aett_logo_slider.png)
+      no-repeat center 50%;
+    background-size: 100% auto;
+  }
+  
+  .owl-controls {
+    position: absolute;
+    margin-top: 300px;
+  }
+  */
